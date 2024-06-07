a7d5f0be934f6023cd998bca69d2ff93ceff2de2
mathiery <marc.thiery04@gmail.com>
2024-06-07 10:50:52 +0200

    répartition des données

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from src.model.load import loadCSVBase
 from src.model import norm_stand as norm
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     #baseView()
     dataset = loadCSVBase('./trees.csv');
-    print(norm.normalizeMinMax(dataset));
-    print(norm.normalizeMaxAbs(dataset));
-    print(norm.normalizeRobust(dataset));
-    print(norm.standardisationZScore(dataset));
-    #norm.normalizeImg(img);
+    for ds in dataset:
+        plt.hist(dataset[ds])
+        plt.title("répartition des données de : " + ds)
+        plt.show()
