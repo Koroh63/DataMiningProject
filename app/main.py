b4cb6efa2ec122bcdf39c18aadbe96d9f1e76bb4
Corentin R <corentin.richard@etu.uca.fr>
2024-06-07 12:00:25 +0200

    Revert "Merge pull request 'normalisation' (#13) from

--- app/main.py
+++ app/main.py
@@ -1,11 +1,4 @@
-from src.model.load import loadCSVBase
-from src.model import norm_stand as norm
+from src.view.index import baseView
 
 if __name__ == "__main__":
-    #baseView()
-    dataset = loadCSVBase('./trees.csv');
-    print(norm.normalizeMinMax(dataset));
-    print(norm.normalizeMaxAbs(dataset));
-    print(norm.normalizeRobust(dataset));
-    print(norm.standardisationZScore(dataset));
-    #norm.normalizeImg(img);
+    baseView()
